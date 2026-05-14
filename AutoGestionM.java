72dc0211400e89827a6c748375eb920d29e9dbcd
benjamenndez <benjamendez717@gmail.com>
2026-05-14 00:46:34 -0300

    git add . git commit -m "Agrego gestion de estudiantes en materias" git push origin interfaz

--- AutoGestionM.java
+++ AutoGestionM.java
@@ -10,6 +10,57 @@ import java.util.Scanner;
 public class AutoGestionM {
     static Scanner sc = new Scanner(System.in);
     static List<Materia> materias = new ArrayList<>();
+    public static Materia buscarMateria(int codigo) {
+
+    for(Materia m : materias) {
+
+        if(m.getCodigo() == codigo) {
+            return m;
+        }
+    }
+
+    return null;
+}
+    public static void inscribirEstudiante() {
+
+    System.out.print("Codigo de la materia: ");
+    int codigo = sc.nextInt();
+    sc.nextLine();
+
+    Materia m = buscarMateria(codigo);
+
+    if(m != null) {
+
+        System.out.print("Nombre del estudiante: ");
+        String nombre = sc.nextLine();
+
+        m.inscribirEstudiante(nombre);
+
+        System.out.println("Estudiante inscripto.");
+
+    } else {
+
+        System.out.println("Materia no encontrada.");
+    }
+}
+    public static void mostrarEstudiantesMateria() {
+
+    System.out.print("Codigo de la materia: ");
+    int codigo = sc.nextInt();
+
+    Materia m = buscarMateria(codigo);
+
+    if(m != null) {
+
+        System.out.println("Materia: " + m.getNombre());
+
+        m.mostrarEstudiantes();
+
+    } else {
+
+        System.out.println("Materia no encontrada.");
+    }
+}
     public static void main(String[] args) {
         int opcion;
 
@@ -18,6 +69,8 @@ public class AutoGestionM {
             System.out.println("\n--- SISTEMA ---");
             System.out.println("1. Crear materia");
             System.out.println("2. Mostrar materias");
+            System.out.println("3. Inscribir estudiante");
+            System.out.println("4. Mostrar estudiantes");
             System.out.println("0. Salir");
 
             opcion = sc.nextInt();
@@ -32,6 +85,13 @@ public class AutoGestionM {
                 case 2:
                     mostrarMaterias();
                     break;
+                    case 3:
+                    inscribirEstudiante();
+                    break;
+
+                    case 4:
+                    mostrarEstudiantesMateria();
+                     break;
 
                 case 0:
                     System.out.println("Saliendo...");
